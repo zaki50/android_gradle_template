6251d8255398a6c52aa98f56321f0f4d2a4ab668
Makoto Yamazaki <zaki@uphyca.com>
2013-06-06 20:30:52 +0900

    introduced AndroidAnnotations
    Android Studio にプロジェクトをインポートして使用する場合は以下の手順で apt の factory の設定と source path の追加が必要です。
    
    Preferences... の Compiler > Annotation Processors で Enable annotation processing のチェックボックスにチェックを入れ、Obtain processors from project classpath を選択する
    MyTemplate プロジェクトを選択し、 File > Project Structure を開く。 Modules ＞MyTemplate を選択し Sources タブを選んで build/source/apt_generated/debug を選択し、上にある Sourcesボタン を押して Sourceフォルダとする。

--- MyTemplate/src/main/java/com/example/mytemplate/MainActivity.java
+++ MyTemplate/src/main/java/com/example/mytemplate/MainActivity.java
@@ -1,23 +1,11 @@
 package com.example.mytemplate;
 
-import android.os.Bundle;
 import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.OptionsMenu;
 
+@EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.main)
 public class MainActivity extends SherlockActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getSupportMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
