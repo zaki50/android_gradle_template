568694d84ec4f0400805de9975ddc96274524f96
Makoto Yamazaki <zaki@uphyca.com>
2014-07-08 13:36:42 +0900

    minSdkVersion を 15に変更し、 AppCompat を使わないようにしました。

--- MyApplication/src/main/java/com/example/mytemplate/MainActivity.java
+++ MyApplication/src/main/java/com/example/mytemplate/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.mytemplate;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import butterknife.ButterKnife;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
